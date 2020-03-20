745ea9bc442ab03737cec5ab52cf0f76ed31cd58
Miguel Muscat <miguelmuscat93@gmail.com>
2020-03-20 23:28:02 +0100

    Added `Ty` helper class

---
+++ src/Ty.php
@@ -0,0 +1,206 @@
+<?php
+
+namespace Dhii\Structs;
+
+use Dhii\Structs\PropTypes\ArrayPropType;
+use Dhii\Structs\PropTypes\BoolPropType;
+use Dhii\Structs\PropTypes\CallablePropType;
+use Dhii\Structs\PropTypes\FloatPropType;
+use Dhii\Structs\PropTypes\ObjectPropType;
+use Dhii\Structs\PropTypes\IntersectionPropType;
+use Dhii\Structs\PropTypes\IntPropType;
+use Dhii\Structs\PropTypes\NullablePropType;
+use Dhii\Structs\PropTypes\StringPropType;
+use Dhii\Structs\PropTypes\UnionPropType;
+use TypeError;
+
+/**
+ * Helper methods for easily declaring property types.
+ *
+ * Some of the below methods use a static cache to conserve memory. Types that require or can accept arguments will not
+ * have their instances cached.
+ *
+ * Also provides additional utility methods for property type implementations.
+ *
+ * @since [*next-version*]
+ */
+class Ty
+{
+    /**
+     * Integer property type.
+     *
+     * @since [*next-version*]
+     *
+     * @return IntPropType
+     */
+    public static function int() : IntPropType
+    {
+        static $ty = null;
+        is_null($ty) && $ty = new IntPropType();
+
+        return $ty;
+    }
+
+    /**
+     * Float property type.
+     *
+     * @since [*next-version*]
+     *
+     * @return FloatPropType
+     */
+    public static function float() : FloatPropType
+    {
+        static $ty = null;
+        is_null($ty) && $ty = new FloatPropType();
+
+        return $ty;
+    }
+
+    /**
+     * Boolean property type.
+     *
+     * @since [*next-version*]
+     *
+     * @return BoolPropType
+     */
+    public static function bool() : BoolPropType
+    {
+        static $ty = null;
+        is_null($ty) && $ty = new BoolPropType();
+
+        return $ty;
+    }
+
+    /**
+     * String property type.
+     *
+     * @since [*next-version*]
+     *
+     * @return StringPropType
+     */
+    public static function string() : StringPropType
+    {
+        static $ty = null;
+        is_null($ty) && $ty = new StringPropType();
+
+        return $ty;
+    }
+
+    /**
+     * Callable property type.
+     *
+     * @since [*next-version*]
+     *
+     * @return CallablePropType
+     */
+    public static function callable() : CallablePropType
+    {
+        static $ty = null;
+        is_null($ty) && $ty = new CallablePropType();
+
+        return $ty;
+    }
+
+    /**
+     * Array property type.
+     *
+     * @since [*next-version*]
+     *
+     * @return ArrayPropType
+     */
+    public static function array() : ArrayPropType
+    {
+        static $ty = null;
+        is_null($ty) && $ty = new ArrayPropType();
+
+        return $ty;
+    }
+
+    /**
+     * Object property type.
+     *
+     * @since [*next-version*]
+     *
+     * @param string|null $what The name of the class to extend or interface to implement.
+     *
+     * @return ObjectPropType
+     */
+    public static function object(string $what = null) : ObjectPropType
+    {
+        return new ObjectPropType($what);
+    }
+
+    /**
+     * Type-union property type.
+     *
+     * @since [*next-version*]
+     *
+     * @param PropType ...$types The union types.
+     *
+     * @return UnionPropType
+     */
+    public static function union(PropType ...$types)
+    {
+        return new UnionPropType($types);
+    }
+
+    /**
+     * Type-intersection property type.
+     *
+     * @since [*next-version*]
+     *
+     * @param PropType ...$types The intersection types.
+     *
+     * @return IntersectionPropType
+     */
+    public static function intersect(PropType ...$types)
+    {
+        return new IntersectionPropType($types);
+    }
+
+    /**
+     * Nullable property type.
+     *
+     * @since [*next-version*]
+     *
+     * @param PropType $type The nullable type.
+     *
+     * @return NullablePropType
+     */
+    public static function nullable(PropType $type)
+    {
+        return new NullablePropType($type);
+    }
+
+    /**
+     * Retrieves the name of the type for a given value.
+     *
+     * @since [*next-version*]
+     *
+     * @param mixed $value The value.
+     *
+     * @return string the name of the type.
+     */
+    public static function getTypeName($value)
+    {
+        return is_object($value) ? get_class($value) : gettype($value);
+    }
+
+    /**
+     * Creates a type error for invalid struct property values.
+     *
+     * @since [*next-version*]
+     *
+     * @param PropType $type  The property type.
+     * @param mixed    $value The value.
+     *
+     * @return TypeError The created type error.
+     */
+    public static function createTypeError(PropType $type, $value) : TypeError
+    {
+        $typeName = $type->getName();
+        $valType = static::getTypeName($value);
+
+        return new TypeError("Property value must be of type {$typeName}, {$valType} given");
+    }
+}
---
+++ test/func/TyFuncTest.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace Dhii\Structs\Tests\Func;
+
+use Dhii\Structs\PropType;
+use Dhii\Structs\PropTypes\ArrayPropType;
+use Dhii\Structs\PropTypes\BoolPropType;
+use Dhii\Structs\PropTypes\CallablePropType;
+use Dhii\Structs\PropTypes\FloatPropType;
+use Dhii\Structs\PropTypes\ObjectPropType;
+use Dhii\Structs\PropTypes\IntersectionPropType;
+use Dhii\Structs\PropTypes\IntPropType;
+use Dhii\Structs\PropTypes\NullablePropType;
+use Dhii\Structs\PropTypes\StringPropType;
+use Dhii\Structs\PropTypes\UnionPropType;
+use Dhii\Structs\Ty;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use ReflectionException;
+
+/**
+ * @since [*next-version*]
+ */
+class TyFuncTest extends TestCase
+{
+    /**
+     * @since [*next-version*]
+     */
+    public function testInt()
+    {
+        static::assertInstanceOf(IntPropType::class, Ty::int());
+    }
+
+    /**
+     * @since [*next-version*]
+     */
+    public function testFloat()
+    {
+        static::assertInstanceOf(FloatPropType::class, Ty::float());
+    }
+
+    /**
+     * @since [*next-version*]
+     */
+    public function testBool()
+    {
+        static::assertInstanceOf(BoolPropType::class, Ty::bool());
+    }
+
+    /**
+     * @since [*next-version*]
+     */
+    public function testString()
+    {
+        static::assertInstanceOf(StringPropType::class, Ty::string());
+    }
+
+    /**
+     * @since [*next-version*]
+     */
+    public function testCallable()
+    {
+        static::assertInstanceOf(CallablePropType::class, Ty::callable());
+    }
+
+    /**
+     * @since [*next-version*]
+     */
+    public function testArray()
+    {
+        static::assertInstanceOf(ArrayPropType::class, Ty::array());
+    }
+
+    /**
+     * @since [*next-version*]
+     */
+    public function testObject()
+    {
+        static::assertInstanceOf(ObjectPropType::class, Ty::object());
+    }
+
+    /**
+     * @since [*next-version*]
+     *
+     * @throws ReflectionException
+     */
+    public function testUnion()
+    {
+        /* @var $inner1 MockObject&PropType */
+        /* @var $inner2 MockObject&PropType */
+        /* @var $inner3 MockObject&PropType */
+        $inner1 = $this->getMockForAbstractClass(PropType::class);
+        $inner2 = $this->getMockForAbstractClass(PropType::class);
+        $inner3 = $this->getMockForAbstractClass(PropType::class);
+
+        static::assertInstanceOf(UnionPropType::class, Ty::union($inner1, $inner2, $inner3));
+    }
+
+    /**
+     * @since [*next-version*]
+     *
+     * @throws ReflectionException
+     */
+    public function testIntersection()
+    {
+        /* @var $inner1 MockObject&PropType */
+        /* @var $inner2 MockObject&PropType */
+        /* @var $inner3 MockObject&PropType */
+        $inner1 = $this->getMockForAbstractClass(PropType::class);
+        $inner2 = $this->getMockForAbstractClass(PropType::class);
+        $inner3 = $this->getMockForAbstractClass(PropType::class);
+
+        static::assertInstanceOf(IntersectionPropType::class, Ty::intersect($inner1, $inner2, $inner3));
+    }
+
+    /**
+     * @since [*next-version*]
+     *
+     * @throws ReflectionException
+     */
+    public function testNullable()
+    {
+        /* @var $inner MockObject&PropType */
+        $inner = $this->getMockForAbstractClass(PropType::class);
+
+        static::assertInstanceOf(NullablePropType::class, Ty::nullable($inner));
+    }
+}
